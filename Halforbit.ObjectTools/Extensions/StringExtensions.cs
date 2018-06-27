3f39123a3e138b24f9c3614e46e1541486b1237e
Jim Noble <none@none>
2018-06-27 18:08:55 -0400

    add stringextensions

---
+++ Halforbit.ObjectTools/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Halforbit.ObjectTools.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string JoinString<TValue>(
+            this IEnumerable<TValue> values, 
+            string separator)
+        {
+            return string.Join(separator, values);
+        }
+    }
+}
