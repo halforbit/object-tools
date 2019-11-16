7794e98ebfe4b317dfe58ccd32241dc03c81a3d9
Jim Noble <jimnoble@gmail.com>
2019-11-15 19:57:09 -0500

    add naming conversion extensions

---
+++ Halforbit.ObjectTools.Tests/Naming/NamingExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace Halforbit.ObjectTools.Tests.Naming
+{
+    public class NamingExtensionsTests
+    {
+        [Fact, Trait("Type", "Unit")]
+        public void TrainToPascalCase_Success()
+        {
+            var actual = "alfa-bravo.charlie-delta".TrainToPascalCase();
+
+            Assert.Equal("AlfaBravo.CharlieDelta", actual);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void PascalToTrainCase_Success()
+        {
+            var actual = "AlfaBravo.CharlieDelta".PascalToTrainCase();
+
+            Assert.Equal("alfa-bravo.charlie-delta", actual);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void TrainToCamelCase_Success()
+        {
+            var actual = "alfa-bravo.charlie-delta".TrainToCamelCase();
+
+            Assert.Equal("alfaBravo.charlieDelta", actual);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void CamelToTrainCase_Success()
+        {
+            var actual = "alfaBravo.charlieDelta".CamelToTrainCase();
+
+            Assert.Equal("alfa-bravo.charlie-delta", actual);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void PascalToCamelCase_Success()
+        {
+            var actual = "AlfaBravo.CharlieDelta".TrainToCamelCase();
+
+            Assert.Equal("alfaBravo.charlieDelta", actual);
+        }
+
+        [Fact, Trait("Type", "Unit")]
+        public void CamelToPascalCase_Success()
+        {
+            var actual = "alfaBravo.charlieDelta".CamelToPascalCase();
+
+            Assert.Equal("AlfaBravo.CharlieDelta", actual);
+        }
+    }
+}
---
+++ Halforbit.ObjectTools/Naming/NamingExtensions.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Halforbit.ObjectTools
+{
+    public static class NamingExtensions
+    {
+        static Regex _wordMatcher = new Regex(
+            @"([A-Z]+(?![a-z]))|([0-9]+)|([A-Z][a-z]+)|([a-z]+)|\.", 
+            RegexOptions.Compiled);
+
+        public static string TrainToPascalCase(
+            this string source, 
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+
+            return string.Join(
+                string.Empty,
+                GetWords(source, retainDots)
+                    .Select(w => $"{char.ToUpper(w[0])}{(w.Length > 1 ? w.Substring(1).ToLower() : string.Empty)}"));
+        }
+
+        public static string PascalToTrainCase(
+            this string source,
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+
+            var sb = new StringBuilder();
+
+            var first = true;
+
+            var prevWord = default(string);
+
+            foreach(var word in GetWords(source, retainDots))
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else if(word != "." && prevWord != ".")
+                {
+                    sb.Append("-");
+                }
+
+                sb.Append(word.ToLower());
+
+                prevWord = word;
+            }
+
+            return sb.ToString();
+        }
+
+        public static string TrainToCamelCase(
+            this string source,
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+
+            var sb = new StringBuilder();
+
+            var first = true;
+
+            foreach (var word in GetWords(source, retainDots))
+            {
+                if (first)
+                {
+                    sb.Append(word.ToLower());
+
+                    first = false;
+                }
+                else if (word == ".")
+                {
+                    sb.Append(word);
+
+                    first = true;
+                }
+                else
+                {
+                    sb.Append($"{char.ToUpper(word[0])}{(word.Length > 1 ? word.Substring(1).ToLower() : string.Empty)}");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string CamelToTrainCase(
+            this string source,
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+
+            var sb = new StringBuilder();
+
+            var first = true;
+
+            var prevWord = default(string);
+
+            foreach (var word in GetWords(source, retainDots))
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else if (word != "." && prevWord != ".")
+                {
+                    sb.Append("-");
+                }
+
+                sb.Append(word.ToLower());
+
+                prevWord = word;
+            }
+
+            return sb.ToString();
+        }
+
+        public static string PascalToCamelCase(
+            this string source,
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+
+            var sb = new StringBuilder();
+
+            var first = true;
+
+            foreach (var word in GetWords(source, retainDots))
+            {
+                if (first)
+                {
+                    sb.Append(word.ToLower());
+
+                    first = false;
+                }
+                else if (word == ".")
+                {
+                    sb.Append(word);
+
+                    first = true;
+                }
+                else
+                {
+                    sb.Append($"{char.ToUpper(word[0])}{(word.Length > 1 ? word.Substring(1).ToLower() : string.Empty)}");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string CamelToPascalCase(
+            this string source,
+            bool retainDots = true)
+        {
+            if (string.IsNullOrWhiteSpace(source)) return source;
+            
+            var sb = new StringBuilder();
+
+            var first = true;
+
+            return string.Join(
+                string.Empty, 
+                GetWords(source, retainDots)
+                    .Select(w => $"{char.ToUpper(w[0])}{(w.Length > 1 ? w.Substring(1).ToLower() : string.Empty)}"));
+        }
+
+        static IEnumerable<string> GetWords(
+            string source, 
+            bool retainDots)
+        {
+            var matches = _wordMatcher
+                .Matches(source)
+                .Cast<Match>()
+                .Select(m => m.Value)
+                .Where(w => retainDots || w != ".")
+                .ToList();
+
+            return matches;
+        }
+    }
+}
