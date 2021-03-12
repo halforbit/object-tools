e747acc798893eb2c43d62a60dfc7114bb29f7e8
Jim Noble <jim@halforbit.com>
2021-03-12 16:24:28 -0500

    new validation methods

---
+++ Halforbit.ObjectTools.Tests/Validation/ValidationErrorsTests.cs
@@ -0,0 +1,58 @@
+﻿using Halforbit.ObjectTools.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Halforbit.ObjectTools.Tests.Validation
+{
+    public class ValidationErrorsTests
+    {
+        [Fact]
+        public void Regarding_Require()
+        {
+            var person = new Person("Steve", string.Empty);
+
+            var result = ValidationErrors.Empty
+                .Regarding(person)
+                .Require(p => p.FirstName)
+                .Require(p => p.LastName);
+
+            Assert.Equal(1, result.Count);
+
+            Assert.Equal("LastName required.", result[0].Message);
+        }
+
+        [Fact]
+        public void Regarding_Require_Anonymous()
+        {
+            var person = new Person("Steve", string.Empty);
+
+            var result = ValidationErrors.Empty
+                .Regarding(person)
+                .Require(p => new
+                {
+                    p.FirstName,
+                    p.LastName
+                });
+
+            Assert.Equal(1, result.Count);
+
+            Assert.Equal("LastName required.", result[0].Message);
+        }
+    }
+
+    public class Person
+    {
+        public Person(
+            string firstName,
+            string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+    }
+}
--- Halforbit.ObjectTools/Validation/ValidationErrors.cs
+++ Halforbit.ObjectTools/Validation/ValidationErrors.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Halforbit.ObjectTools.Validation
 {
@@ -31,6 +34,20 @@ namespace Halforbit.ObjectTools.Validation
         public override string ToString() => string.Join("\r\n", _validationErrors);
 
         IEnumerator IEnumerable.GetEnumerator() => _validationErrors.GetEnumerator();
+
+        public ValidationErrors<TItem> Regarding<TItem>(TItem item) => new ValidationErrors<TItem>(item, _validationErrors);
+    }
+
+    public class ValidationErrors<TItem> : ValidationErrors
+    {
+        internal ValidationErrors(
+            TItem item, 
+            IReadOnlyList<ValidationError> validationErrors) : base(validationErrors) 
+        {
+            Item = item;
+        }
+
+        public TItem Item { get; }
     }
 
     public static class ValidationErrorsExtensions
@@ -61,8 +78,44 @@ namespace Halforbit.ObjectTools.Validation
             return new ValidationErrors(newValidationErrors);
         }
 
+        public static ValidationErrors<TItem> With<TItem>(
+            this ValidationErrors<TItem> source,
+            ValidationError validationError)
+        {
+            var validationErrors = new List<ValidationError>(source.Count + 1);
+
+            validationErrors.AddRange(source);
+
+            validationErrors.Add(validationError);
+
+            return new ValidationErrors<TItem>(source.Item, validationErrors);
+        }
+
+        public static ValidationErrors<TItem> With<TItem>(
+            this ValidationErrors<TItem> source,
+            IReadOnlyList<ValidationError> validationErrors)
+        {
+            var newValidationErrors = new List<ValidationError>(source.Count + validationErrors.Count);
+
+            newValidationErrors.AddRange(source);
+
+            newValidationErrors.AddRange(validationErrors);
+
+            return new ValidationErrors<TItem>(source.Item, newValidationErrors);
+        }
+
         public static ValidationErrors If(
             this ValidationErrors source,
+            bool predicate,
+            ValidationError validationError)
+        {
+            return predicate ?
+                source.With(validationError) :
+                source;
+        }
+
+        public static ValidationErrors<TItem> If<TItem>(
+            this ValidationErrors<TItem> source,
             bool predicate,
             ValidationError validationError)
         {
@@ -75,6 +128,13 @@ namespace Halforbit.ObjectTools.Validation
             this ValidationErrors source,
             TValue value,
             string name)
+        {
+            return ValueIsMissing(value) ?
+                source.With(ValidationError.Required(name)) :
+                source;
+        }
+
+        static bool ValueIsMissing<TValue>(TValue value)
         {
             var valueType = typeof(TValue);
 
@@ -98,18 +158,96 @@ namespace Halforbit.ObjectTools.Validation
             {
                 missing = true;
             }
-            else if (enumerableInterface != null)
-            {
-                missing = (int)valueType.GetProperty(nameof(IReadOnlyCollection<object>.Count)).GetValue(value) == 0;
-            }
+            //else if (enumerableInterface != null)
+            //{
+            //    missing = (int)valueType.GetProperty(nameof(IReadOnlyCollection<object>.Count)).GetValue(value) == 0;
+            //}
             else
             {
                 missing = value.IsDefaultValue();
             }
 
-            return missing ?
-                source.With(ValidationError.Required(name)) :
-                source;
+            return missing;
         }
+
+        public static ValidationErrors<TItem> Require<TItem, TValue>(
+            this ValidationErrors<TItem> source,
+            Expression<Func<TItem, TValue>> propertySelector)
+        {
+            var value = propertySelector.Compile()(source.Item);
+
+            var valueType = typeof(TValue);
+
+            if (IsAnonymousType(valueType))
+            {
+                foreach (var property in valueType.GetProperties())
+                {
+                    var propertyValue = property.GetValue(value);
+
+                    var propertyMissing = (bool)typeof(ValidationErrorsExtensions)
+                        .GetMethod(nameof(ValueIsMissing), BindingFlags.NonPublic | BindingFlags.Static)
+                        .MakeGenericMethod(property.PropertyType)
+                        .Invoke(default(object), new object[] { propertyValue });
+
+                    if (propertyMissing)
+                    {
+                        source = source.With($"{property.Name} required.");
+                    }
+                }
+            }
+            else if (ValueIsMissing(value))
+            {
+                var propertyInfo = GetPropertyInfo(propertySelector);
+
+                return source.With($"{propertyInfo.Name} required.");
+            }
+
+            return source;
+        }
+
+        static PropertyInfo GetPropertyInfo<TSource, TProperty>(
+            Expression<Func<TSource, TProperty>> propertyLambda)
+        {
+            Type type = typeof(TSource);
+
+            MemberExpression member;
+
+            if (propertyLambda.Body is UnaryExpression u)
+            {
+                member = u.Operand as MemberExpression;
+            }
+            else
+            {
+                member = propertyLambda.Body as MemberExpression;
+            }
+
+            if (member == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propertyLambda.ToString()));
+
+            PropertyInfo propInfo = member.Member as PropertyInfo;
+            if (propInfo == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propertyLambda.ToString()));
+
+            if (type != propInfo.ReflectedType &&
+                !type.IsSubclassOf(propInfo.ReflectedType))
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property that is not from type {1}.",
+                    propertyLambda.ToString(),
+                    type));
+
+            return propInfo;
+        }
+
+        static bool IsAnonymousType(Type type) => Attribute.IsDefined(
+            type, typeof(CompilerGeneratedAttribute), false) && 
+                type.IsGenericType && 
+                type.Name.Contains("AnonymousType") && 
+                (type.Name.StartsWith("<>", StringComparison.OrdinalIgnoreCase) ||
+                    type.Name.StartsWith("VB$", StringComparison.OrdinalIgnoreCase)) && 
+                (type.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
     }
 }
